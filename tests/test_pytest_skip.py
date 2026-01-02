31ce8c9151fbce810c2cad6b7c3666b175b251b4
DmitriyFrolov2 <dmitrii.frolov2018@yandex.ru>
2026-01-03 00:54:26 +0300

    Pytest skip, skipif, xfail

---
+++ tests/test_pytest_skip.py
@@ -0,0 +1,17 @@
+import pytest
+
+
+@pytest.mark.skip(reason="Фича в разработке")
+def test_feature_in_development():
+    ...
+
+
+@pytest.mark.skip
+class TestSuiteSkp:
+    def test_feature_in_development_1(self):
+        ...
+
+    def test_feature_in_development_2(self):
+        ...
+
+
