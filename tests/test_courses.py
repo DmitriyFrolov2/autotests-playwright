ce8ef7df357b8fb10df95c737c32fc9974693e69
DmitriyFrolov2 <dmitrii.frolov2018@yandex.ru>
2026-01-04 16:27:11 +0300

    Плагины Pytest

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,20 +1,23 @@
-from playwright.sync_api import expect
 import pytest
+from playwright.sync_api import expect, Page
 
 
+@pytest.mark.courses
 @pytest.mark.regression
-@pytest.mark.authorization
-def test_empty_courses_list(chromium_page_with_state):
+def test_empty_courses_list(chromium_page_with_state: Page):
     chromium_page_with_state.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
 
-    courses_page_title = chromium_page_with_state.get_by_test_id('courses-list-toolbar-title-text')
-    expect(courses_page_title).to_be_visible()
-    expect(courses_page_title).to_have_text('Courses')
+    courses_title = chromium_page_with_state.get_by_test_id('courses-list-toolbar-title-text')
+    expect(courses_title).to_be_visible()
+    expect(courses_title).to_have_text('Courses')
 
-    empty_state_title = chromium_page_with_state.get_by_test_id('courses-list-empty-view-title-text')
-    expect(empty_state_title).to_be_visible()
-    expect(empty_state_title).to_have_text('There is no results')
+    empty_view_icon = chromium_page_with_state.get_by_test_id('courses-list-empty-view-icon')
+    expect(empty_view_icon).to_be_visible()
 
-    empty_state_description = chromium_page_with_state.get_by_test_id('courses-list-empty-view-description-text')
-    expect(empty_state_description).to_be_visible()
-    expect(empty_state_description).to_have_text('Results from the load test pipeline will be displayed here')
+    empty_view_title = chromium_page_with_state.get_by_test_id('courses-list-empty-view-title-text')
+    expect(empty_view_title).to_be_visible()
+    expect(empty_view_title).to_have_text('There is no results')
+
+    empty_view_description = chromium_page_with_state.get_by_test_id('courses-list-empty-view-description-text')
+    expect(empty_view_description).to_be_visible()
+    expect(empty_view_description).to_have_text('Results from the load test pipeline will be displayed here')
