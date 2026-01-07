d3e315edd12fead7950670fbb562a60fdb201cc2
DmitriyFrolov2 <dmitrii.frolov2018@yandex.ru>
2026-01-08 01:55:48 +0300

    Практикуемся в написании автотестов с использованием POM Реализация test_create_course

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,15 +1,36 @@
 import pytest
+from playwright.sync_api import Page, expect
 from pages.courses_list_page import CoursesListPage
 from pages.create_course_page import CreateCoursePage
 
 
 @pytest.mark.courses
 @pytest.mark.regression
-def test_create_course(
-    create_course_page: CreateCoursePage,
-    courses_list_page: CoursesListPage
-):
+def test_empty_courses_list(chromium_page_with_state: Page):
+    chromium_page_with_state.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
 
+    courses_title = chromium_page_with_state.get_by_test_id('courses-list-toolbar-title-text')
+    expect(courses_title).to_be_visible()
+    expect(courses_title).to_have_text('Courses')
+
+    empty_view_icon = chromium_page_with_state.get_by_test_id('courses-list-empty-view-icon')
+    expect(empty_view_icon).to_be_visible()
+
+    empty_view_title = chromium_page_with_state.get_by_test_id('courses-list-empty-view-title-text')
+    expect(empty_view_title).to_be_visible()
+    expect(empty_view_title).to_have_text('There is no results')
+
+    empty_view_description = chromium_page_with_state.get_by_test_id('courses-list-empty-view-description-text')
+    expect(empty_view_description).to_be_visible()
+    expect(empty_view_description).to_have_text('Results from the load test pipeline will be displayed here')
+
+
+@pytest.mark.courses
+@pytest.mark.regression
+def test_create_course(
+        create_course_page: CreateCoursePage,
+        courses_list_page: CoursesListPage
+):
     courses_list_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create")
     create_course_page.check_visible_create_course_title()
 
@@ -23,6 +44,7 @@ def test_create_course(
         max_score="0",
         min_score="0"
     )
+
     create_course_page.check_visible_exercises_title()
     create_course_page.check_visible_create_exercise_button()
     create_course_page.check_visible_exercises_empty_view()
