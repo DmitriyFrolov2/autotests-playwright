38ff94692bd400d3c0df40a44d1ba1e569853dae
DmitriyFrolov2 <dmitrii.frolov2018@yandex.ru>
2026-01-07 23:22:40 +0300

    Описание страницы CreateCoursePage

--- fixtures/pages.py
+++ fixtures/pages.py
@@ -2,9 +2,10 @@ import pytest
 from playwright.sync_api import Page
 
 from pages.courses_list_page import CoursesListPage
+from pages.create_course_page import CreateCoursePage
+from pages.dashboard_page import DashboardPage
 from pages.login_page import LoginPage
 from pages.registration_page import RegistrationPage
-from pages.dashboard_page import DashboardPage
 
 
 @pytest.fixture
@@ -25,3 +26,8 @@ def dashboard_page(chromium_page: Page) -> DashboardPage:
 @pytest.fixture
 def courses_list_page(chromium_page_with_state: Page) -> CoursesListPage:
     return CoursesListPage(page=chromium_page_with_state)
+
+
+@pytest.fixture
+def create_course_page(chromium_page_with_state: Page) -> CreateCoursePage:
+    return CreateCoursePage(page=chromium_page_with_state)
