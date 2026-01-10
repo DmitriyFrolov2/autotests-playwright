6b9c29bd8c906f7a4533dd39cca6759b580a5f97
DmitriyFrolov2 <dmitrii.frolov2018@yandex.ru>
2026-01-10 20:54:50 +0300

    Знакомство с PageFactory Работа с динамическими локаторами в компонентах

---
+++ elements/input.py
@@ -0,0 +1,17 @@
+from playwright.sync_api import Locator, expect
+
+from elements.base_element import BaseElement
+
+# Методы сппецифичные для полей ввода
+class Input(BaseElement):
+    def get_locator(self, **kwargs) -> Locator:
+        return super().get_locator(**kwargs).locator('input')
+
+    def fill(self, value: str, **kwargs): # метод, который заполняет поле ввода указанным текстом.
+        locator = self.get_locator(**kwargs)
+        locator.fill(value)
+
+    def check_have_value(self, value: str, **kwargs): # метод, который проверяет значение, находящееся в поле ввода
+        locator = self.get_locator(**kwargs)
+        expect(locator).to_have_value(value)
+
