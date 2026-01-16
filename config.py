1ddc28096c1d5279eefa970ae62f0d8cd672e3bd
DmitriyFrolov2 <dmitrii.frolov2018@yandex.ru>
2026-01-16 13:52:25 +0300

    Добавляем информацию о переменных окружения в Allure отчет

--- config.py
+++ config.py
@@ -36,6 +36,7 @@ class Settings(BaseSettings):
     test_data: TestData
     videos_dir: DirectoryPath
     tracing_dir: DirectoryPath
+    allure_results_dir: DirectoryPath
     browser_state_file: FilePath
 
     def get_base_url(self) -> str:
@@ -47,19 +48,21 @@ class Settings(BaseSettings):
         # Указываем пути
         videos_dir = DirectoryPath("./videos")
         tracing_dir = DirectoryPath("./tracing")
+        allure_results_dir = DirectoryPath("./allure-results")
         browser_state_file = FilePath("browser_state.json")
 
         # Создаем директории, если они не существуют
         videos_dir.mkdir(exist_ok=True)  # Если директория существует, то игнорируем ошибку
         tracing_dir.mkdir(exist_ok=True)
-        # Создаем файл состояния браузера, если его нет
+        allure_results_dir.mkdir(exist_ok=True)
         browser_state_file.touch(exist_ok=True)  # Если файл существует, то игнорируем ошибку
 
         # Возвращаем модель с инициализированными значениями
         return Settings(
             videos_dir=videos_dir,
             tracing_dir=tracing_dir,
-            browser_state_file=browser_state_file
+            allure_results_dir=allure_results_dir,
+            browser_state_file=browser_state_file,
         )
 
 # Инициализируем настройки
